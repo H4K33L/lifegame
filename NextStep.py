fee7fe8d02f717ffef5301805fee600b73949593
Jeremy <jeremy.gaudin@ynov.com>
2024-11-21 15:34:01 +0100

    🎨 Imporved gameManager, nextStep still in progress

--- NextStep.py
+++ NextStep.py
@@ -1,10 +1,41 @@
 class NextStep:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+   
+   def __init__(self, grid):
+      self.NextStep(self, grid)
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+   def NextStep(self, grid):
+      newgrid = grid
+      aliveCellsList = self.GetAliveCellsList(self.grid)
+      for aliveCell in aliveCellsList:
+         neighbors = self.GetNeighborCells(self, grid, aliveCell[0], aliveCell[1])
+         nAliveNeighbors = 0
+         for neighbor in neighbors:
+            if grid[neighbor[0]][neighbor[1]] == 1:
+               nAliveNeighbors += 1
+         if nAliveNeighbors not in [2,3]:
+            newgrid[aliveCell[0]][aliveCell[1]] == 0
+         
+             
+            
+          
+          
+   def GetAliveCellsList(self, grid):
+      aliveCellsList = []
+    
+      for i in range(len(grid)):
+         for j in range(len(grid)):
+            if grid[i][j] == 1:
+               aliveCellsList.append((i,j))
 
-p1 = Person("John", 36)
-p1.myfunc()
+      return aliveCellsList
+    
+   def GetNeighborCells(self, grid, x, y):
+      neighbors = [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)]
+
+      validNeighbors = []
+
+      for neighborCell in neighbors:
+         if 0 <= neighborCell[0] < len(grid) and 0 <= neighborCell[1] < len(grid):
+            validNeighbors.append(neighborCell)
+      
+      return validNeighbors
