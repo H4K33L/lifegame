a19b9a1e958c6aa541ab499f44260b0f9a07ce92
Jeremy <jeremy.gaudin@ynov.com>
2024-11-21 16:29:47 +0100

    🚀 Finished frist version of CycleDetection.py + comments

---
+++ CycleDetection.py
@@ -0,0 +1,26 @@
+class CycleDetection:
+   
+    def __init__(self, grid, history):
+        self.CycleDetection(self, grid, history)
+        
+    def CycleDetection(self, grid, history):
+        currentAliveCellsList = self.GetAliveCellsList(grid)
+        for previousGrid in history:
+            if len(currentAliveCellsList) == len(previousGrid):
+                if self.ListEquals(currentAliveCellsList,previousGrid):
+                    return True
+        return False
+    
+    def ListEquals (list1, list2):
+        return list1 == list2
+    
+    # Get all alive cells from the grid     
+    def GetAliveCellsList(self, grid):
+        aliveCellsList = []
+    
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                if grid[i][j] == 1:
+                    aliveCellsList.append((i,j))
+                    
+        return aliveCellsList
